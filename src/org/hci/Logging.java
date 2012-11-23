92f5968a66adc06bf69a1ea2cd47c46e5147de24
Derek Bekoe <dbbekoe@gmail.com>
2012-11-23 12:34:33 +0000

    Created main screen

---
+++ src/org/hci/Logging.java
@@ -0,0 +1,6 @@
+package org.hci;
+
+//class for saving data from the program in the correct format
+public class Logging {
+
+}
--- src/org/hci/MainWindow.java
+++ src/org/hci/MainWindow.java
@@ -2,23 +2,87 @@ package org.hci;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import java.util.*;
 import javax.swing.*;
+import java.awt.color.*;
 
-public class MainWindow extends JPanel implements KeyListener, MouseListener {
+public class MainWindow extends JPanel implements Runnable, KeyListener, MouseListener {
+	
+	BufferedImage buffer;
+	static int width;
+	static int height;
+	int states;
 	
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
+		MainWindow w = new MainWindow();
+		JFrame frame = new JFrame("HCI - Group Floor 2pi");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setUndecorated(true);
+		//frame.setUndecorated(true); //removes top bar with close buttons
 		frame.setLocationByPlatform(true);
 		frame.setResizable(false);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setSize(screenSize);
+		width = screenSize.width;
+		height = screenSize.height;
 		
 		
-		frame.add();
+		frame.add(w);
 		
-		frame.pack();
+		//frame.pack();
 		frame.setVisible(true);
+		SwingUtilities.invokeLater(w);
+		w.mainLoop();
+	}
+	
+	public MainWindow() {
+		this.setBackground(Color.black);
+	}
+	
+	public void run(){
+
+	}
+	
+	void mainLoop() {
+		states = 0;
+		while (true)
+			draw();
+	}
+	
+	void draw() {
+		buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		Graphics2D b = buffer.createGraphics();
+		Graphics2D g = (Graphics2D) this.getGraphics();
+		switch (states) {
+			case 0:
+			b = drawStartScreen(b);
+			break;
+			case 1:
+			b = drawExperiment(b);
+			break;
+		}
+		g.drawImage(buffer, 0, 0, this);
+		Toolkit.getDefaultToolkit().sync();
+		b.dispose();
+		g.dispose();
+	}
+	
+	Graphics2D drawStartScreen(Graphics2D b) {
+		b.setColor(Color.WHITE);
+		b.setFont(new Font(Font.SERIF, Font.PLAIN, 72));
+		b.drawString("Instructions", centreAlignString("Instructions", b), 100);
+		b.drawString("During this, move the mouse", centreAlignString("During this, move the mouse", b), 300);
+		b.drawString("to the center circle to start.", centreAlignString("to the center circle to start.", b), 400);
+		b.setFont(new Font(Font.SERIF, Font.PLAIN, 60));
+		b.drawString("Press space key to begin.", centreAlignString("Press space key to begin.", b), 600);
+		return b;
+	}
+	
+	int centreAlignString(String s, Graphics2D g) {
+		FontMetrics f = g.getFontMetrics();
+		Rectangle2D rect = f.getStringBounds(s, g);
+		return (int) ((width-rect.getWidth())/2);
 	}
 
 	@Override
@@ -60,6 +124,8 @@ public class MainWindow extends JPanel implements KeyListener, MouseListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
+		//if key is SPACE key, then go to experiment
+		if (states==0)
 		
 	}
 
